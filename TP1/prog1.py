76f5d5f94a4f355474a004aacecfe054657b4383
TheoFreret <frereto1990@gmail.com>
2023-09-08 14:14:59 +0200

    sauveguarde_2

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,4 +1,13 @@
+import fonctions as f
+
 print("Hello, World!")
+
+print("calcule de a^b :")
+v1 = int(input("Valleur de a : "))
+v2 = int(input("Valleur de b : "))
+
+res = f.puissance(v1,v2)
+
 while True :
 	Nb = int(input("Entrer un nombre a mettre au carree : "))
 	NbPower = pow(Nb, 2)
